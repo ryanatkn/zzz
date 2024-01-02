59ea4a51c95e2d2024f5c41a625afc2daa72aab6
Ryan Atkinson <mail@ryanatkn.com>
2024-01-01 17:18:01 -0700

    Initial commit

---
+++ src/routes/Settings.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Color_Scheme_Input from '@fuz.dev/fuz/Color_Scheme_Input.svelte';
+	import Theme_Input from '@fuz.dev/fuz/Theme_Input.svelte';
+</script>
+
+<div class="prose padded_1 box">
+	<h1>Settings</h1>
+	<h2>Color Scheme</h2>
+	<Color_Scheme_Input />
+	<h2>Theme</h2>
+	<Theme_Input />
+</div>
