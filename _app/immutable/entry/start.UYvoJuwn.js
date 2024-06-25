116af6128a61e28a2e0b14b7dc733371db961535
Ryan Atkinson <mail@ryanatkn.com>
2024-06-25 08:32:04 -0600

    deployment

---
+++ _app/immutable/entry/start.UYvoJuwn.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.KA3rHhmN.js";export{t as start};
---
+++ _app/immutable/nodes/2.jwZkBpac.js
@@ -0,0 +1,30 @@
+import{a as w,n as _,c as i,t as f,b as l}from"../chunks/disclose-version.h0Z-2GrA.js";import{s as p,a as d,b as x}from"../chunks/style.Ha1ESfTK.js";import{t as u}from"../chunks/runtime._lAnztwU.js";import{p as c}from"../chunks/props.jDj64oyT.js";var z=_(`<svg><path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02
+		15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32
+		12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12
+		11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33
+		11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74
+		5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95
+		7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31
+		12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12
+		7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01
+		13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71
+		14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" transform="scale(64)"></path></svg>`);function C(o,t){const s=c(t,"size",3,"var(--space_xl7, 64px)"),e=c(t,"fill",3,"var(--text_color, #000)"),r=c(t,"label",3,"the GitHub icon, an octocat silhouette");var a=z();let n;var h=i(a);u(()=>{n=p(a,n,{xmlns:"http://www.w3.org/2000/svg","aria-label":r(),viewBox:"0 0 1024 1024",fill:"none",...t.attrs},!1,""),d(a,"width",s()),d(a,"height",s()),x(h,"fill",e())}),w(o,a)}var y=_("<svg><path></path></svg>");function Z(o,t){const s=c(t,"size",3,"100%"),e=c(t,"fill",3,"var(--text_color, #000)"),r=c(t,"label",3,"a little brown spider");var a=y();let n;var h=i(a);let v;u(()=>{n=p(a,n,{xmlns:"http://www.w3.org/2000/svg","aria-label":r(),viewBox:"0 0 100 100",width:s(),height:s(),fill:"none",...t.attrs},!1,""),v=p(h,v,{fill:e(),d:`M 26.253917,88.532336 29.904106,71.394855 40.667193,53.342811 40.258534,49.99234 38.417407,
+		49.000991 22.876908,50.369035 9.4865496,53.880193 2.3019024,57.978424 0.42708056,
+		57.27994 7.2642726,51.086985 20.811326,45.373351 37.960128,42.356792 39.354818,
+		40.107008 38.229925,38.149883 26.030989,27.105568 14.46539,21.861786 8.0479986,
+		18.615387 l -0.41428,-1.710463 8.2789464,1.499862 13.012873,5.003724 13.447448,
+		10.696856 1.680801,-0.729547 0.222439,-1.343157 -3.983998,-12.128053 -5.730215,
+		-9.573597 -0.823624,-5.1744052 1.16944,-1.165102 2.604334,6.3355162 6.612025,7.08777 4.874534,
+		11.55989 2.800804,0.515574 4.48815,-1.359246 1.521623,-8.687062 5.685014,-8.620764 2.75965,
+		-6.8316782 1.094578,1.128569 -1.293029,5.4222362 -4.084776,11.06803 -0.484994,
+		8.377408 0.194311,1.192896 1.42954,1.700726 11.563936,-10.644623 9.878262,-8.331535 8.732915,
+		-3.390708 -0.387305,1.402757 -5.294686,3.023816 -10.445054,10.705792 -9.561599,
+		13.627899 -0.438945,1.602755 1.001398,1.666754 17.376932,3.986302 9.537375,6.940531 4.325785,
+		4.636405 0.211208,1.557106 -6.15842,-4.279925 -10.413771,-5.155697 -15.838715,
+		-1.696223 -0.83461,1.144484 0.774499,2.593247 9.737644,16.194355 3.925704,17.214082 0.07146,
+		10.277289 -1.706242,1.13628 -2.009721,-9.21637 -5.894265,-16.88027 -12.292087,
+		-17.295813 -4.177778,-0.585888 -7.294671,2.935716 -11.138052,16.645915 -6.462422,
+		17.752509 -1.634756,7.206641 -2.070766,-1.52923 z`,...t.path_attrs},!1,"")}),w(o,a)}var S=f('<span class="inline_block"><!></span>');function q(o,t){const s=c(t,"label",3,"the Zzz logo, a little pink spider");var e=S();d(e,"--text_color","var(--color_g_5)");var r=i(e);Z(r,{get label(){return s()}}),u(()=>{d(e,"width",t.size),d(e,"height",t.size)}),w(o,e)}var B=f(`<main class="width_sm svelte-1uktwki"><section class="box"><header class="box"><h1 class="mt_xl mb_xl2">Zzz</h1> <blockquote class="shadow_inset_xs bg px_xl">social toolz for the tired <a href="https://github.com/ryanatkn/zzz" title="View source" class="svelte-1uktwki">💤</a></blockquote> <!></header></section> <section class="panel p_xl shadow_inset_sm"><div class="panel bg p_md shadow_sm"><p>Zzz (pronounced "zees" like please) is a social web app framework that builds on <a href="https://kit.svelte.dev/" class="svelte-1uktwki">SvelteKit</a> and several of <a href="https://www.ryanatkn.com/" class="svelte-1uktwki">my projects</a>. It's not yet published
+				but should be in 2024 and I plan to make videos and blog about it here soon.</p> <p>Join <a href="https://discord.gg/YU5tyeK72X" class="svelte-1uktwki">the Discord</a> and subscribe on YouTube:</p> <ul><li><a href="https://www.youtube.com/@spiderspace_8000" class="svelte-1uktwki">@spiderspace_8000</a> - nontechnical
+					design videos where we'll make a social website together with Zzz (<a href="https://www.spiderspace.org/" class="svelte-1uktwki">spiderspace.org</a>)</li> <li><a href="https://www.youtube.com/@webdevladder" class="svelte-1uktwki">@webdevladder</a> - advanced realworld
+					webdev videos with TypeScript and Svelte (blog: <a href="https://www.webdevladder.net/" class="svelte-1uktwki">webdevladder.net</a>)</li></ul> <p>See how Zzz fits with my other software on <a href="https://www.ryanatkn.com/" class="svelte-1uktwki">my homepage</a>.</p></div></section> <div class="shadow_outset_xs p_xl radius_lg mb_xl box"><footer class="box"><a class="box text_align_center px_xl py_md panel shadow_outset_xs svelte-1uktwki" href="https://github.com/ryanatkn/zzz" title="View source"><div class="mb_xs">source code</div> <!></a></footer></div></main>`);function M(o){var t=B(),s=i(t),e=i(s),r=i(e),a=l(l(r,!0)),n=l(l(a,!0));q(n,{size:"var(--icon_size_xl)"});var h=l(l(s,!0)),v=l(l(h,!0)),m=i(v),b=i(m);d(b,"display","flex");var k=i(b),g=l(l(k,!0));C(g,{}),w(o,t)}export{M as component};
