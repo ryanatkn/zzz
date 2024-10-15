15686543b6a2002e7503a48b097eb496e0de5e52
Ryan Atkinson <mail@ryanatkn.com>
2024-10-14 21:37:29 -0600

    wip

---
+++ src/lib/id.ts
@@ -0,0 +1,12 @@
+import type {Flavored} from '@ryanatkn/belt/types.js';
+
+export type Id = Flavored<number, 'Id'>;
+
+// TODO faster? use crypto for randomization?
+export const random_id = (): Id => {
+	let v = parseInt(Math.random().toString().substring(2), 10);
+	while (v > Number.MAX_SAFE_INTEGER) {
+		v = Math.round(v / 2);
+	}
+	return v;
+};
