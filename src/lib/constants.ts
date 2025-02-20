c6433d0e1ed337107ed7513c9bbd018b4928061a
Ryan Atkinson <mail@ryanatkn.com>
2025-02-20 16:23:06 -0700

    wip

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const SYMBOL_CHAT = '⌸';
 export const SYMBOL_TAPE = '≣';
 export const SYMBOL_FILE = '⏚'; // ⏚ ⧈ ⊞ ⧉ ⌺ ▤
 export const SYMBOL_PROMPTS = '⌇'; // ⌇ ⍋  ⌭
-export const SYMBOL_PROVIDER = '⨕'; // TODO something more readable? ⧰ 🜢 ⍌ ⌗ ⌾ ⨖
+export const SYMBOL_PROVIDER = '⨕';
 export const SYMBOL_MODEL = '⊛'; // ⨹ ⨺ ⊛
 // export const SYMBOL_ABOUT = '⍾'; // using Zzz logo instead - ⎎ ⍰ ? ⍾
 export const SYMBOL_CAPABILITIES = '⧰'; // ⌁
@@ -14,3 +14,24 @@ export const SYMBOL_SETTINGS = '⛮'; // ⛭  ⚙  ⛮  ⛯ ⛣
 // ⧛
 
 // ⚟
+
+// ⌬ ⬡ ⬢ ⏣
+
+// ⍝
+
+// 🜢
+
+// ⨖
+
+// ⌾
+// ⌓
+
+// ⌯
+// ⌗
+// ⎍
+
+// ⎕
+// ⌘
+
+// ⌥
+// ⌤
