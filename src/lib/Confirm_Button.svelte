5c058bd77d794670acb80838247ad49b2d0ef11d
Ryan Atkinson <mail@ryanatkn.com>
2025-02-22 06:23:48 -0700

    wip

--- src/lib/Confirm_Button.svelte
+++ src/lib/Confirm_Button.svelte
@@ -19,29 +19,14 @@
 	let confirming = $state(false);
 	const toggle = () => (confirming = !confirming);
 
-	// TODO ideally there would be an active state visible on the button when confirming, but `selected` is too visually heavy
+	// TODO BLOCK make the active state visible on the button when `confirming=true`, but `selected` is too visually heavy
 
 	// TODO changing the font size works if there's no children, but that's a weird difference - the UX is broken for custom buttons because they change size when the font size changes
+
+	// TODO add contextmenu behavior to dismiss the confirmation button
 </script>
 
 <div class="relative">
-	{#if confirming}
-		<button
-			type="button"
-			class="color_c absolute icon_button bg_c_1"
-			style:left="calc(-1 * var(--input_height))"
-			style:transform-origin="right"
-			onclick={() => {
-				confirming = false;
-				onclick();
-			}}
-			in:scale={{duration: 80}}
-			out:scale={{duration: 200}}
-			{...attrs}
-		>
-			🗙
-		</button>
-	{/if}
 	{#if button}
 		{@render button(confirming, toggle)}
 	{:else}
@@ -54,6 +39,24 @@
 			{...button_attrs}
 		>
 			{#if children}{@render children(confirming)}{:else}🗙{/if}
+		</button>
+	{/if}
+	{#if confirming}
+		<button
+			type="button"
+			class="color_c absolute icon_button bg_c_1"
+			style:left="calc(-1 * var(--input_height))"
+			style:top="0"
+			style:transform-origin="right"
+			onclick={() => {
+				confirming = false;
+				onclick();
+			}}
+			in:scale={{duration: 80}}
+			out:scale={{duration: 200}}
+			{...attrs}
+		>
+			🗙
 		</button>
 	{/if}
 </div>
