e073d928d058ea726d45c80c1cd52c9ecc21f694
Ryan Atkinson <mail@ryanatkn.com>
2025-02-23 02:59:45 -0700

    wip

--- src/lib/Prompt_Fragment_File_Controls.svelte
+++ src/lib/Prompt_Fragment_File_Controls.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import {scale} from 'svelte/transition';
+
 	import type {Prompt_Fragment} from '$lib/prompt.svelte.js';
 	import type {Prompts} from '$lib/prompts.svelte.js';
 
@@ -20,12 +22,14 @@
 		is file
 	</label>
 	{#if fragment.is_file}
-		<input
-			type="text"
-			class="flex_1"
-			placeholder="/path/to/file"
-			value={fragment.file_path}
-			oninput={(e) => prompts.update_fragment(fragment.id, {file_path: e.currentTarget.value})}
-		/>
+		<div in:scale={{duration: 80}} out:scale={{duration: 200}} style:transform-origin="center left">
+			<input
+				type="text"
+				class="flex_1"
+				placeholder="/path/to/file"
+				value={fragment.file_path}
+				oninput={(e) => prompts.update_fragment(fragment.id, {file_path: e.currentTarget.value})}
+			/>
+		</div>
 	{/if}
 </div>
