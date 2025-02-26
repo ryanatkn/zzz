4b98e84d67a48fb53cc083cc976c73a40a82c05e
Ryan Atkinson <mail@ryanatkn.com>
2025-02-25 19:26:01 -0700

    wip

--- src/lib/Bit_Stats.svelte
+++ src/lib/Bit_Stats.svelte
@@ -11,7 +11,9 @@
 
 <div class="flex justify_content_space_between gap_sm font_mono">
 	<span title="the token count is currently approximated and inaccurate"
-		>{print_number_with_separators(token_count + '', ',')} token{token_count === 1 ? '' : 's'}</span
+		>~{print_number_with_separators(token_count + '', ',')} token{token_count === 1
+			? ''
+			: 's'}</span
 	>
 	<span>{print_number_with_separators(length + '', ',')} char{length === 1 ? '' : 's'}</span>
 </div>
