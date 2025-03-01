3182fe44afd975e9e2a15b02c233b6b8e82cff3c
Ryan Atkinson <mail@ryanatkn.com>
2025-03-01 05:37:23 -0700

    wip

--- src/lib/zod_helpers.ts
+++ src/lib/zod_helpers.ts
@@ -7,8 +7,7 @@ import {EMPTY_ARRAY} from '@ryanatkn/belt/array.js';
 export const zod_get_schema_keys = <T extends z.ZodTypeAny>(schema: T): Array<string> => {
 	if (schema instanceof z.ZodObject) {
 		// For ZodObject, we can access the shape to get the keys
-		const shape = schema._def.shape();
-		return Object.keys(shape);
+		return Object.keys(schema._def.shape());
 	} else if (schema instanceof z.ZodEffects) {
 		// For ZodEffects (like transforms), get keys from the inner schema
 		return zod_get_schema_keys(schema.innerType());
