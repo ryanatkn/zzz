0677464c6d79706b47296045fde8b52108754327
Ryan Atkinson <mail@ryanatkn.com>
2025-02-28 23:48:38 -0700

    wip

--- src/lib/serializable.svelte.ts
+++ src/lib/serializable.svelte.ts
@@ -2,7 +2,7 @@
 
 import type {Json_Object} from '@ryanatkn/belt/json.js';
 
-export abstract class Serializable<T_Json> {
+export abstract class Serializable<T_Json extends Json_Object> {
 	json: T_Json = $derived.by(() => this.to_json());
 	json_serialized: string = $derived(JSON.stringify(this.json));
 
