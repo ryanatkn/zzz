1702cd806f5625d4a6ad09e2130284493a1f5179
Ryan Atkinson <mail@ryanatkn.com>
2025-03-20 16:12:47 -0600

    wip

--- src/lib/Capability_System.svelte
+++ src/lib/Capability_System.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import {DEV} from 'esm-env';
 	import {base} from '$app/paths';
+	import {format_url} from '@ryanatkn/belt/url.js';
 
 	import {pkg_context} from '$routes/pkg.js';
 
@@ -12,5 +13,6 @@
 	<p class="font_mono">
 		DEV: {DEV + ''}
 	</p>
+	<p><a href={pkg.repo_url}>{format_url(pkg.repo_url)}</a></p>
 	<p><a href="{base}/about">/about</a></p>
 </div>
