8b4c56200e2093cb3df6e30cc16a8a21aa9d2826
Ryan Atkinson <mail@ryanatkn.com>
2025-04-04 06:25:36 -0600

    wip

--- src/lib/Dashboard_Settings.svelte
+++ src/lib/Dashboard_Settings.svelte
@@ -2,8 +2,11 @@
 	import {GLYPH_SETTINGS} from '$lib/glyphs.js';
 	import Glyph from '$lib/Glyph.svelte';
 	import Settings from '$lib/Settings.svelte';
+	import {zzz_context} from '$lib/zzz.svelte.js';
 
 	import Footer from '$routes/Footer.svelte';
+
+	const zzz = zzz_context.get();
 
 	// TODO display capabilities (like what APIs are available, including remote server (off when deployed statically), local pglite (could be disconnected, websockets?))
 	// TODO display database info/explorer
@@ -15,6 +18,26 @@
 	</header>
 	<section class="width_sm">
 		<Settings />
+
+		<div class="mt_lg">
+			<h3>ui</h3>
+			<div class="my_md">
+				<label class="row">
+					<input type="checkbox" bind:checked={zzz.ui.show_sidebar} aria-label="show sidebar" />
+					show sidebar <code class="ml_md">[backtick `]</code>
+				</label>
+			</div>
+			<div class="my_md">
+				<label class="row">
+					<input
+						type="checkbox"
+						bind:checked={zzz.ui.show_main_dialog}
+						aria-label="show main dialog"
+					/>
+					show main dialog
+				</label>
+			</div>
+		</div>
 	</section>
 	<section class="mb_xl7 flex justify_content_center">
 		<Footer />
