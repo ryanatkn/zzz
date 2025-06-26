d9abd825c4bad34e39fbc904b054688a96264d63
Ryan Atkinson <mail@ryanatkn.com>
2025-06-26 00:35:29 -0400

    wip

--- src/lib/Contextmenu_Chats.svelte
+++ src/lib/Contextmenu_Chats.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 	import type {ComponentProps} from 'svelte';
 	import Contextmenu from '@ryanatkn/fuz/Contextmenu.svelte';
-	import type {Partial_Only} from '@ryanatkn/belt/types.js';
+	import type {Omit_Strict} from '@ryanatkn/belt/types.js';
 	import Contextmenu_Entry from '@ryanatkn/fuz/Contextmenu_Entry.svelte';
 
 	import {frontend_context} from '$lib/frontend.svelte.js';
 	import {GLYPH_CHAT} from '$lib/glyphs.js';
 	import Glyph from '$lib/Glyph.svelte';
 
-	type Props = Partial_Only<ComponentProps<typeof Contextmenu>, 'entries'>;
+	type Props = Omit_Strict<ComponentProps<typeof Contextmenu>, 'entries'>;
 
 	const props: Props = $props();
 
 	const app = frontend_context.get();
 </script>
 
-<Contextmenu {...props} entries={props.entries ?? entries} />
+<Contextmenu {...props} {entries} />
 
 {#snippet entries()}
 	<Contextmenu_Entry run={() => app.chats.add(undefined, true)}>
--- src/lib/Ollama_Model_Details.svelte
+++ src/lib/Ollama_Model_Details.svelte
@@ -26,183 +26,179 @@
 	};
 
 	// TODO refactor with `Model_Detail`?
-
-	// TODO BLOCK upstream Contextmenu_Model to have `attrs` and a prop for the element tag
 </script>
 
-<Contextmenu_Model {model}>
-	<div class="panel p_md">
-		<header class="display_flex justify_content_space_between mb_md">
-			<div class="display_flex flex_column gap_xs">
-				<h3 class="mt_0 mb_0 font_family_mono">
-					<Model_Link {model} icon />
-				</h3>
-				<div>
-					{model.filesize ? Math.round(model.filesize * 1024) : '?'} GB
-				</div>
-				<div class="font_family_mono">
-					modified {format_short_date(model.ollama_modified_at) || '--'}
-				</div>
+<Contextmenu_Model attrs={{class: 'panel p_md'}} {model}>
+	<header class="display_flex justify_content_space_between mb_md">
+		<div class="display_flex flex_column gap_xs">
+			<h3 class="mt_0 mb_0 font_family_mono">
+				<Model_Link {model} icon />
+			</h3>
+			<div>
+				{model.filesize ? Math.round(model.filesize * 1024) : '?'} GB
 			</div>
+			<div class="font_family_mono">
+				modified {format_short_date(model.ollama_modified_at) || '--'}
+			</div>
+		</div>
 
-			{#if onclose}
-				<button type="button" class="icon_button plain" onclick={onclose} title="close">
-					<Glyph glyph={GLYPH_ARROW_LEFT} />
-				</button>
-			{/if}
-		</header>
+		{#if onclose}
+			<button type="button" class="icon_button plain" onclick={onclose} title="close">
+				<Glyph glyph={GLYPH_ARROW_LEFT} />
+			</button>
+		{/if}
+	</header>
 
-		<section class="display_flex gap_sm mb_md">
+	<section class="display_flex gap_sm mb_md">
+		<button
+			type="button"
+			class="plain"
+			onclick={() => {
+				// TODO get `app` from context? or is this fine? should it be protected? generic types might make it a no-go
+				const chat = ollama.app.chats.add(undefined, true);
+				chat.add_tape(model);
+			}}
+		>
+			<Glyph glyph={GLYPH_ADD} attrs={{class: 'mr_xs2'}} /> create a new chat
+		</button>
+
+		{#if model.ollama_details_loaded}
 			<button
 				type="button"
 				class="plain"
-				onclick={() => {
-					// TODO get `app` from context? or is this fine? should it be protected? generic types might make it a no-go
-					const chat = ollama.app.chats.add(undefined, true);
-					chat.add_tape(model);
-				}}
+				title="clear cache and reload details"
+				onclick={() => ollama.refresh_model_details(model.name)}
 			>
-				<Glyph glyph={GLYPH_ADD} attrs={{class: 'mr_xs2'}} /> create a new chat
+				<Glyph glyph={GLYPH_REFRESH} />&nbsp; reload details
 			</button>
-
-			{#if model.ollama_details_loaded}
+		{:else if model.needs_ollama_details}
+			<div class="display_flex gap_sm align_items_center">
 				<button
 					type="button"
 					class="plain"
-					title="clear cache and reload details"
-					onclick={() => ollama.refresh_model_details(model.name)}
+					onclick={() => load_model_details()}
+					title="load model details"
 				>
-					<Glyph glyph={GLYPH_REFRESH} />&nbsp; reload details
+					<Glyph glyph={GLYPH_REFRESH} />&nbsp; load details
 				</button>
-			{:else if model.needs_ollama_details}
-				<div class="display_flex gap_sm align_items_center">
+			</div>
+		{/if}
+
+		{#if ondelete}
+			<Confirm_Button
+				onconfirm={() => ondelete(model.name)}
+				position="right"
+				attrs={{
+					class: 'plain color_c',
+					title: `delete ${model.name}`,
+				}}
+			>
+				<Glyph glyph={GLYPH_DELETE} />&nbsp; delete model
+
+				{#snippet popover_content(popover)}
 					<button
 						type="button"
-						class="plain"
-						onclick={() => load_model_details()}
-						title="load model details"
+						class="color_c icon_button bg_c_1"
+						title="confirm delete"
+						onclick={() => {
+							// TODO async confirmation
+							ondelete(model.name);
+							popover.hide();
+						}}
 					>
-						<Glyph glyph={GLYPH_REFRESH} />&nbsp; load details
+						<Glyph glyph={GLYPH_DELETE} />
 					</button>
+				{/snippet}
+			</Confirm_Button>
+		{/if}
+	</section>
+
+	{#if model.ollama_details_loading}
+		<section class="display_flex gap_sm align_items_center">
+			<Pending_Animation />
+			<span class="font_size_sm">loading model details...</span>
+		</section>
+	{:else if model.ollama_details_error}
+		<section class="display_flex flex_column gap_sm">
+			<div class="color_c font_size_sm">
+				failed to load details: {model.ollama_details_error}
+			</div>
+			<button
+				type="button"
+				class="color_c icon_button plain"
+				onclick={() => load_model_details()}
+				title="retry loading details"
+			>
+				<Glyph glyph={GLYPH_REFRESH} />
+			</button>
+		</section>
+	{:else if model.ollama_details}
+		<section class="display_flex flex_column gap_md">
+			<!-- Basic Info -->
+			{#if model.ollama_details.details}
+				<div class="display_grid gap_sm" style:grid-template-columns="auto 1fr">
+					<h5 class="my_0">family:</h5>
+					<span class="font_family_mono">{model.ollama_details.details.family}</span>
+
+					<h5 class="my_0">format:</h5>
+					<span class="font_family_mono">{model.ollama_details.details.format}</span>
+
+					<h5 class="my_0">parameters:</h5>
+					<span class="font_family_mono">
+						{model.ollama_details.details.parameter_size}
+					</span>
+
+					<h5 class="my_0">quantization:</h5>
+					<span class="font_family_mono">
+						{model.ollama_details.details.quantization_level}
+					</span>
+
+					{#if model.ollama_details.details.parent_model}
+						<h5 class="my_0">parent:</h5>
+						<span class="font_family_mono">{model.ollama_details.details.parent_model}</span>
+					{/if}
 				</div>
 			{/if}
 
-			{#if ondelete}
-				<Confirm_Button
-					onconfirm={() => ondelete(model.name)}
-					position="right"
-					attrs={{
-						class: 'plain color_c',
-						title: `delete ${model.name}`,
-					}}
-				>
-					<Glyph glyph={GLYPH_DELETE} />&nbsp; delete model
+			<!-- System Prompt -->
+			{#if model.ollama_details.system}
+				<div>
+					<h5>system prompt:</h5>
+					<pre><code>{model.ollama_details.system}</code></pre>
+				</div>
+			{/if}
 
-					{#snippet popover_content(popover)}
-						<button
-							type="button"
-							class="color_c icon_button bg_c_1"
-							title="confirm delete"
-							onclick={() => {
-								// TODO async confirmation
-								ondelete(model.name);
-								popover.hide();
-							}}
-						>
-							<Glyph glyph={GLYPH_DELETE} />
-						</button>
-					{/snippet}
-				</Confirm_Button>
+			<!-- Template -->
+			{#if model.ollama_details.template}
+				<div>
+					<h5>template:</h5>
+					<pre><code>{model.ollama_details.template}</code></pre>
+				</div>
+			{/if}
+
+			<!-- Model Info -->
+			{#if model.ollama_details.model_info && Object.keys(model.ollama_details.model_info).length > 0}
+				<div>
+					<h5>model info:</h5>
+					<pre><code>{JSON.stringify(model.ollama_details, null, '\t')}</code></pre>
+				</div>
+			{/if}
+
+			<!-- License -->
+			{#if model.ollama_details.license}
+				<div>
+					<h5>license:</h5>
+					<pre><code>{model.ollama_details.license}</code></pre>
+				</div>
+			{/if}
+
+			<!-- Modelfile -->
+			{#if model.ollama_details.modelfile}
+				<div>
+					<h5>modelfile:</h5>
+					<pre><code>{model.ollama_details.modelfile}</code></pre>
+				</div>
 			{/if}
 		</section>
-
-		{#if model.ollama_details_loading}
-			<section class="display_flex gap_sm align_items_center">
-				<Pending_Animation />
-				<span class="font_size_sm">loading model details...</span>
-			</section>
-		{:else if model.ollama_details_error}
-			<section class="display_flex flex_column gap_sm">
-				<div class="color_c font_size_sm">
-					failed to load details: {model.ollama_details_error}
-				</div>
-				<button
-					type="button"
-					class="color_c icon_button plain"
-					onclick={() => load_model_details()}
-					title="retry loading details"
-				>
-					<Glyph glyph={GLYPH_REFRESH} />
-				</button>
-			</section>
-		{:else if model.ollama_details}
-			<section class="display_flex flex_column gap_md">
-				<!-- Basic Info -->
-				{#if model.ollama_details.details}
-					<div class="display_grid gap_sm" style:grid-template-columns="auto 1fr">
-						<h5 class="my_0">family:</h5>
-						<span class="font_family_mono">{model.ollama_details.details.family}</span>
-
-						<h5 class="my_0">format:</h5>
-						<span class="font_family_mono">{model.ollama_details.details.format}</span>
-
-						<h5 class="my_0">parameters:</h5>
-						<span class="font_family_mono">
-							{model.ollama_details.details.parameter_size}
-						</span>
-
-						<h5 class="my_0">quantization:</h5>
-						<span class="font_family_mono">
-							{model.ollama_details.details.quantization_level}
-						</span>
-
-						{#if model.ollama_details.details.parent_model}
-							<h5 class="my_0">parent:</h5>
-							<span class="font_family_mono">{model.ollama_details.details.parent_model}</span>
-						{/if}
-					</div>
-				{/if}
-
-				<!-- System Prompt -->
-				{#if model.ollama_details.system}
-					<div>
-						<h5>system prompt:</h5>
-						<pre><code>{model.ollama_details.system}</code></pre>
-					</div>
-				{/if}
-
-				<!-- Template -->
-				{#if model.ollama_details.template}
-					<div>
-						<h5>template:</h5>
-						<pre><code>{model.ollama_details.template}</code></pre>
-					</div>
-				{/if}
-
-				<!-- Model Info -->
-				{#if model.ollama_details.model_info && Object.keys(model.ollama_details.model_info).length > 0}
-					<div>
-						<h5>model info:</h5>
-						<pre><code>{JSON.stringify(model.ollama_details, null, '\t')}</code></pre>
-					</div>
-				{/if}
-
-				<!-- License -->
-				{#if model.ollama_details.license}
-					<div>
-						<h5>license:</h5>
-						<pre><code>{model.ollama_details.license}</code></pre>
-					</div>
-				{/if}
-
-				<!-- Modelfile -->
-				{#if model.ollama_details.modelfile}
-					<div>
-						<h5>modelfile:</h5>
-						<pre><code>{model.ollama_details.modelfile}</code></pre>
-					</div>
-				{/if}
-			</section>
-		{/if}
-	</div>
+	{/if}
 </Contextmenu_Model>
