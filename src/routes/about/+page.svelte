0ac4ed1fd9e01d220af619eaefd5718be76cd799
Ryan Atkinson <mail@ryanatkn.com>
2025-07-12 20:56:23 -0400

    wip

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -347,7 +347,8 @@
 				Developers have full control to shoot their own feet, of course, and Node is currently the
 				only supported way to use Zzz. We'll develop a plugin API so people can make reusable
 				integrations, and distribution will be through npm. (decentralized from the project's POV
-				like a typical open source web ecosystem project -- I expect to publish a number of
+				like a typical open source web ecosystem project, I gatekeep nothing -- I expect to publish
+				a number of
 				<code>@ryanatkn/zzz_*</code> packages, and others may do who knows what, you'll have to vet
 				their code)
 				<ul>
