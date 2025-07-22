e2f1eb19f7761ff2b3bd0c3d06fb8015464cddbb
Ryan Atkinson <mail@ryanatkn.com>
2025-07-22 16:26:36 -0400

    wip

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,4 @@
 import {DEV} from 'esm-env';
 
-export const prerender = false; // currently disabled to support dyanmic routes with the static adapter
+export const prerender = false; // currently disabled to support dynamic routes with the static adapter
 export const ssr = DEV;
