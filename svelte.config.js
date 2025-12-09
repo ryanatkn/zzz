717e93c266a02d1b23eaab14e7efbf03abfc6c53
Ryan Atkinson <mail@ryanatkn.com>
2025-12-09 18:59:11 -0500

    disable csp to see if it fixes deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
-import {create_csp_directives} from '@fuzdev/fuz_ui/csp.js';
-import {csp_trusted_sources_of_ryanatkn} from '@fuzdev/fuz_ui/csp_of_ryanatkn.js';
+// TODO debugging
+// import {create_csp_directives} from '@fuzdev/fuz_ui/csp.js';
+// import {csp_trusted_sources_of_ryanatkn} from '@fuzdev/fuz_ui/csp_of_ryanatkn.js';
 
 // Dynamically import adapter based on the ZZZ_BUILD env var.
 // ZZZ_BUILD=node for production Node server, otherwise static for GitHub Pages.
-const build_mode = process.env.ZZZ_BUILD;
-const is_node_build = build_mode === 'node';
+const build_mode_raw = process.env.ZZZ_BUILD;
+// 'static' | 'node'
+const build_mode = build_mode_raw === 'node' ? 'node' : 'static';
 
-const adapter_module = is_node_build
-	? await import('@sveltejs/adapter-node')
-	: await import('@sveltejs/adapter-static');
+const adapter_module =
+	build_mode === 'node'
+		? await import('@sveltejs/adapter-node')
+		: await import('@sveltejs/adapter-static');
 
 const adapter = adapter_module.default;
 
@@ -22,25 +25,25 @@ export default {
 		adapter: adapter(),
 		paths: {relative: false}, // use root-absolute paths for SSR path comparison: https://kit.svelte.dev/docs/configuration#paths
 		alias: {$routes: 'src/routes', '@ryanatkn/zzz': 'src/lib'},
-		csp: {
-			directives: create_csp_directives({
-				trusted_sources: csp_trusted_sources_of_ryanatkn,
-				directives: {
-					'connect-src': [
-						'self',
-						// TODO switch to use env vars
-						'ws://localhost:8999',
-					],
-					'frame-src': [
-						'self',
-						// enable iframing for the example sites
-						'https://css.fuz.dev/',
-						'https://fuz.dev/',
-						'https://*.fuz.dev/',
-					],
-				},
-			}),
-		},
+		// csp: {
+		// 	directives: create_csp_directives({
+		// 		trusted_sources: csp_trusted_sources_of_ryanatkn,
+		// 		directives: {
+		// 			'connect-src': [
+		// 				'self',
+		// 				// TODO switch to use env vars
+		// 				'ws://localhost:8999',
+		// 			],
+		// 			'frame-src': [
+		// 				'self',
+		// 				// enable iframing for the example sites
+		// 				'https://css.fuz.dev/',
+		// 				'https://fuz.dev/',
+		// 				'https://*.fuz.dev/',
+		// 			],
+		// 		},
+		// 	}),
+		// },
 		prerender: {
 			handleUnseenRoutes: 'ignore',
 		},
